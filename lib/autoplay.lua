5e2e865c5a6e7644b136faa1946b0cdb266961d1
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-12-07 22:51:19 +0800

    remove path property check for autoplay

--- lib/autoplay.lua
+++ lib/autoplay.lua
@@ -1,16 +1,14 @@
 -- automatically play when file is loaded
 mp.register_event('file-loaded', function()
-  if mp.get_property('path') then
-    -- start from the beginning if end of file or if the difference
-    -- between the total video time and position is less than a second
-    local duration = mp.get_property_number('duration')
-    local time_pos = mp.get_property_number('time-pos')
-    local eof_reached = mp.get_property('eof-reached') == 'yes'
+  -- start from the beginning if end of file or if the difference
+  -- between the total video time and position is less than a second
+  local duration = mp.get_property_number('duration')
+  local time_pos = mp.get_property_number('time-pos')
+  local eof_reached = mp.get_property('eof-reached') == 'yes'
 
-    if eof_reached or (duration ~= nil and time_pos ~= nil and duration - time_pos < 1) then
-      mp.command('seek 0 absolute')
-    end
-
-    mp.command('set pause no')
+  if eof_reached or (duration ~= nil and time_pos ~= nil and duration - time_pos < 1) then
+    mp.command('seek 0 absolute')
   end
+
+  mp.command('set pause no')
 end)
